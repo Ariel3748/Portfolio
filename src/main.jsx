2e759c50bb30238a7734dc2e3d010af459a448bd
Ariel <ariel.oliva3748@gmail.com>
2025-09-14 23:20:26 -0300

    Configuracion rutas

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./components/App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
